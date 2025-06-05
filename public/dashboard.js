cbbfed56c18d1b073e93d41f5e316301eb9ef805
g3ntl3 <hammedlawal412@gmail.com>
2025-06-06 00:39:22 +0100

    Refactor note display structure in dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -45,14 +45,14 @@ addNote.addEventListener('click', () => {
   displayNotes.innerHTML += `
 
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">${noteEntered.value}</p>
-  </div>
+<div class="card-body">
+<p class="card-text">${noteEntered.value}</p>
+</div>
 </div>
 
 
-  `;
+`;
+// <img src="..." class="card-img-top" alt="...">
 
 
 
