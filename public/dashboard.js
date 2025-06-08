5456fe7f072e7395e0c7c089c421a7e1e722da5d
g3ntl3 <hammedlawal412@gmail.com>
2025-06-08 19:39:21 +0100

    Refactor event listener placement in dashboard.js for improved readability and organization

--- public/dashboard.js
+++ public/dashboard.js
@@ -39,43 +39,42 @@ onAuthStateChanged(auth, (user) => {
     window.location.href = "index.html";
     console.log("user is not signed in");
   }
-});
 
-signOutbtn.addEventListener("click", () => {
-  signOut(auth)
-    .then(() => {
-      console.log("sign out success");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+  signOutbtn.addEventListener("click", () => {
+    signOut(auth)
+      .then(() => {
+        console.log("sign out success");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
-// function for adding note
+  // function for adding note
 
-addNote.addEventListener("click", () => {
-  let databaseRef = ref(database, `noteStorage/${user.uid}`);
-  const date = new Date();
-  let noteObject = {
-    noteEntered: noteEntered.value,
-    nameOfInUser: auth.currentUser.displayName,
-    time: date.toLocaleTimeString(),
-  };
-  push(databaseRef, noteObject);
-  // display info from the databse
+  addNote.addEventListener("click", () => {
+    let databaseRef = ref(database, `noteStorage/${user.uid}`);
+    const date = new Date();
+    let noteObject = {
+      noteEntered: noteEntered.value,
+      nameOfInUser: auth.currentUser.displayName,
+      time: date.toLocaleTimeString(),
+    };
+    push(databaseRef, noteObject);
+    // display info from the databse
 
-  // alert(noteEntered.value)
+    // alert(noteEntered.value)
 
-  // noteEntered.value = " ";
-  // <img src="..." class="card-img-top" alt="...">
-});
-let noteref = ref(database, "noteStorage");
-onValue(noteref, (snapshot) => {
-  let data = snapshot.val();
-  displayNotes.innerHTML = ""; // Clear previous notes
-  if (data) {
-    Object.values(data).forEach((eachNote) => {
-      displayNotes.innerHTML += `
+    // noteEntered.value = " ";
+    // <img src="..." class="card-img-top" alt="...">
+  });
+  let noteref = ref(database, `noteStorage/${user.uid}`);
+  onValue(noteref, (snapshot) => {
+    let data = snapshot.val();
+    displayNotes.innerHTML = ""; // Clear previous notes
+    if (data) {
+      Object.values(data).forEach((eachNote) => {
+        displayNotes.innerHTML += `
         <div class="card" style="width: 18rem;">
           <div class="card-body">
             <h1>${eachNote.nameOfInUser}</h1>
@@ -84,8 +83,9 @@ onValue(noteref, (snapshot) => {
           </div>
         </div>
       `;
-    });
-  } else {
-    displayNotes.innerHTML = "<p>No notes yet.</p>";
-  }
+      });
+    } else {
+      displayNotes.innerHTML = "<p>No notes yet.</p>";
+    }
+  });
 });
