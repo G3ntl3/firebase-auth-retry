6a02416f1a6c0cf16ef78d356ca2f245ae48a8fe
g3ntl3 <hammedlawal412@gmail.com>
2025-06-08 19:13:30 +0100

    Fix formatting of note reference in dashboard.js for consistency

--- public/dashboard.js
+++ public/dashboard.js
@@ -69,7 +69,7 @@ addNote.addEventListener("click", () => {
   // noteEntered.value = " ";
   // <img src="..." class="card-img-top" alt="...">
 });
-let noteref = ref(database, `noteStorage/1`);
+let noteref = ref(database, 'noteStorage/1');
 onValue(noteref, (snapshot) => {
   let data = snapshot.val();
   displayNotes.innerHTML = ""; // Clear previous notes
